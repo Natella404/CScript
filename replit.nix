013f33b3e60e22b7de8d00a644f7d65b23864c13
Natella404 <nathanieldahlstrom@icloud.com>
2024-10-27 21:46:05 +0000

    fixed compatebitly

--- replit.nix
+++ replit.nix
@@ -1,5 +1,24 @@
-{pkgs}: {
-  deps = [
-    pkgs.imagemagick_light
-  ];
+{ pkgs }: {
+    deps = [
+        pkgs.python3
+        pkgs.python3Packages.pip
+        pkgs.gcc
+        pkgs.python3Packages.pybind11
+        pkgs.cmake
+    ];
+
+    # Filtyp-konfiguration för .cript
+    languages.cscript = {
+        extensions = ["cript"];
+        icon = "./cscript/icons/cscript.png";
+        displayName = "CScript";
+    };
+
+    # MIME-typ konfiguration
+    mimeTypes = {
+        "text/x-cscript" = {
+            extensions = ["cript"];
+            icon = "./cscript/icons/cscript.png";
+        };
+    };
 }
--- setup.py
+++ setup.py
@@ -1,28 +1,66 @@
 from setuptools import setup, find_packages, Extension
 import pybind11
+import sys
+import platform
+import os
 
-# Definiera C++-extensions
+def get_environment_config():
+    compile_args = []
+    link_args = []
+
+    # Bas-inställningar för alla miljöer
+    include_dirs = [
+        pybind11.get_include(),
+        pybind11.get_include(user=True)
+    ]
+
+    # Windows-specifika inställningar
+    if platform.system() == "Windows":
+        compile_args = ['/std:c++17']
+
+    # Linux/Unix/MacOS inställningar
+    else:
+        compile_args = ['-std=c++17', '-fPIC']
+        link_args = ['-fPIC']
+
+    # Replit-specifika tillägg
+    if os.getenv('REPL_ID') is not None:
+        include_dirs.append('/nix/store/*/include')
+
+    # VSCode-specifika tillägg
+    if os.getenv('VSCODE_CLI') is not None:
+        compile_args.append('-DVSCODE_ENV')
+
+    return compile_args, link_args, include_dirs
+
+# Hämta miljöspecifika inställningar
+compile_args, link_args, include_dirs = get_environment_config()
+
+# Definiera extensions
 ext_modules = [
     Extension(
         "cscript.core.generator",
         ["cscript/core/generator.cpp"],
-        include_dirs=[pybind11.get_include()],
+        include_dirs=include_dirs,
         language='c++',
-        extra_compile_args=['-std=c++17']
+        extra_compile_args=compile_args,
+        extra_link_args=link_args
     ),
     Extension(
         "cscript.core.parser",
         ["cscript/core/parser.cpp"],
-        include_dirs=[pybind11.get_include()],
+        include_dirs=include_dirs,
         language='c++',
-        extra_compile_args=['-std=c++17']
+        extra_compile_args=compile_args,
+        extra_link_args=link_args
     ),
     Extension(
         "cscript.core.rules",
         ["cscript/core/rules.cpp"],
-        include_dirs=[pybind11.get_include()],
+        include_dirs=include_dirs,
         language='c++',
-        extra_compile_args=['-std=c++17']
+        extra_compile_args=compile_args,
+        extra_link_args=link_args
     )
 ]
 
@@ -32,6 +70,11 @@ setup(
     packages=find_packages(),
     ext_modules=ext_modules,
     install_requires=[
+        'pybind11>=2.6.0',
+        'setuptools>=42.0.0',
+        'wheel>=0.37.0'
+    ],
+    setup_requires=[
         'pybind11>=2.6.0',
         'setuptools>=42.0.0',
         'wheel>=0.37.0'
@@ -47,5 +90,17 @@ setup(
         'console_scripts': [
             'cscript=cscript.runner:main'
         ]
-    }
+    },
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+    ]
 )
