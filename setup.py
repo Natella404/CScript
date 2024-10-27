e34d3f4a08d59e2609c1aec75beef72cd7ebd9f1
Natella404 <nathanieldahlstrom@icloud.com>
2024-10-27 21:14:16 +0000

    fixed packages

--- setup.py
+++ setup.py
@@ -1,33 +1,24 @@
 from setuptools import setup, find_packages, Extension
-import pybind11
-
-ext_modules = [
-    Extension(
-        "cscript.core.generator",
-        ["cscript/core/generator.cpp"],
-        include_dirs=[pybind11.get_include()],
-        language='c++'
-    )
-]
 
 setup(
     name="cscript",
     version="0.1.0",
     packages=find_packages(),
-    ext_modules=ext_modules,
     install_requires=[
         'pybind11>=2.6.0',
         'setuptools>=42.0.0',
         'wheel>=0.37.0'
-    ],
-    setup_requires=[
-        'pybind11>=2.6.0'
     ],
     package_data={
         'cscript': [
             'core/*.cpp',
             'core/*.h',
             'icons/*'
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'cscript=cscript.runner:main'
         ]
     }
 )
