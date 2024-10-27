65f3dd5a5dccef85bdba57f02605af17fcb45739
Natella404 <nathanieldahlstrom@icloud.com>
2024-10-27 21:08:31 +0000

    fixt a thing

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 from setuptools import setup, find_packages, Extension
 import pybind11
-import sys
-import os
 
-# Configure the C++ extension
 ext_modules = [
     Extension(
         "cscript.core.generator",
         ["cscript/core/generator.cpp"],
-        include_dirs=[
-            pybind11.get_include(),
-            pybind11.get_include(user=True)
-        ],
-        language='c++',
-        extra_compile_args=['-std=c++17'],
+        include_dirs=[pybind11.get_include()],
+        language='c++'
     )
 ]
 
@@ -24,40 +17,17 @@ setup(
     ext_modules=ext_modules,
     install_requires=[
         'pybind11>=2.6.0',
+        'setuptools>=42.0.0',
+        'wheel>=0.37.0'
     ],
-    setup_requires=['pybind11>=2.6.0'],
-    package_data={
-        'cscript': ['core/*.cpp', 'core/*.h']
-    },
-    entry_points={
-        'console_scripts': [
-            'cscript=cscript.runner:main'
-        ]
-    }
-)
-# setup.py
-setup(
-    # ... other setup configurations ...
+    setup_requires=[
+        'pybind11>=2.6.0'
+    ],
     package_data={
         'cscript': [
             'core/*.cpp',
             'core/*.h',
-            'icons/*'  # Include icons
-        ]
-    },
-    data_files=[
-        ('icons', ['cscript/icons/cscript.ico',
-                  'cscript/icons/cscript.png'])
-    ]
-)
-
-setup(
-    # ... andra inställningar ...
-    package_data={
-        'cscript': [
-            'core/*.cpp',
-            'core/*.h',
-            'icons/*'  # Detta inkluderar alla ikonfiler
+            'icons/*'
         ]
     }
 )
